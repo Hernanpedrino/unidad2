13e604528c12f30184ef20f50f904e39650ba7bd
Hernan Pedrino <hernanpedrino@gmail.com>
2021-12-21 11:01:16 -0300

    unidad 2 inicio

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DetallesComponent } from './pages/detalles/detalles.component';
+import { CamposComponent } from './pages/login/campos/campos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CatalogoComponent,
+    RegistroComponent,
+    LoginComponent,
+    DetallesComponent,
+    CamposComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
